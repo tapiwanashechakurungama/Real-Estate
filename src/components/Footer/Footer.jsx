5aaf57b6115a3d10054adb2689f9e78f03f75c70
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2024-07-17 09:50:25 +0200

    dont mind my footer :)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,34 @@
-import "./Footer.css"
+import "./Footer.css";
+import { FiHome } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <footer className="footer container">
-      <h2>To be codded</h2>
-    </footer>
-  )
-}
+    <div className="main">
+      <footer className="footer" id="footer">
+        <div className="flex container">
+          <div className="first">
+            <h2><FiHome style={{marginBottom: "5px"}}/>Homez</h2>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia
+              sint laboriosam dolorem iure voluptatem quam quidem molestias. Ad,
+              laboriosam obcaecati?
+            </p>
+          </div>
+          <div className="second">
+            <h2>Links</h2>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+          </div>
+          <div className="last">
+            <h2>History</h2>
+            <p>Copyright @2024 All rights reserved</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, reiciendis.</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import "./Navbar.css"
+import "./Navbar.css";
 import { FiHome } from "react-icons/fi";
 import { LuSend } from "react-icons/lu";
 import { HiOutlineLogin } from "react-icons/hi";
@@ -25,14 +25,34 @@ const Navbar = () => {
           Add property
           <LuSend />
         </a>
-        <div className="hamburger">
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
+        <div
+          className="hamburger"
+          onClick={() => {
+            const body = document.body;
+            event.target.classList.toggle("close");
+            body.classList.toggle("show");
+          }}
+        >
+          <div className="line line1"></div>
+          <div className="line none"></div>
+          <div className="line line2"></div>
+        </div>
+        <div className="phone-ul">
+          <h3>
+            <FiHome />
+            Homez
+          </h3>
+          <ul>
+            <a href="">Home</a>
+            <a href="">Listing</a>
+            <a href="">Property</a>
+            <a href="">Blog</a>
+            <a href="">Pages</a>
+          </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
